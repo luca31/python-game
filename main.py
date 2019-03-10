32be359ff96548a8317dfd70b863cc02dc85cb75
Giovanni Spallanzani <giovi.spallanzani@gmail.com>
2019-03-10 20:42:45 +0100

    Finita classe room che imposta in base al file le dimensioni, creati file 0.room e 1.room

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import json
 
 class Game:
   def __init__(self):
+    #lettura del file game.json
     with open('game.json') as f:
       data = json.load(f)
-
+    #creazione lista di stanze già istanziate all'interno di questa
     self.rooms = []
     self.description = data["description"]
     self.levels = int(data["levels"])
@@ -14,9 +15,24 @@ class Game:
 class Room:
   def __init__(self, n):
     self.n = n
-    # leggere il file n.room
+    self.w = 0
+    self.h = 0
+    #apertura file della stanza
+    file = open(str(n) + ".room")
+    r = file.readlines()
+    file.close()
+    #lettura caratteri del file e assegnazione altezza 
+    l = 0
+    for y in r:
+      for x in y:
+        if x =="\n":
+          self.h += 1
+        else:
+          l += 1
+    #assegnazione larghezza
+    self.w = round(l/ self.h)
 
-
+#istanziata la classe Game
 game = Game()
-
+#printata descrizione del gioco per il giocatore
 print(game.description)
